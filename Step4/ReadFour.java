0e8ad57b1ee188d693400acaa684773b345baac1
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-06 19:37:39 -0400

    I learn how to create a scanner which  will do scanning of the files inside .and how to work with that data

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,8 +1,8 @@
 package Step4;
 
-import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadFour
 {
@@ -10,9 +10,38 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
-            // Print out a running total of all the
-            // values in the input file.
+        public static void main(String[] args) {
+
+            try {
+
+//                System.out.println("Creating a file output.txt");
+//
+//                PrintWriter fileWritter = new PrintWriter("output.txt");
+//
+//                fileWritter.println( "Text inside created file");
+//
+//                System.out.println("File is created ");
+//
+//                fileWritter.close();
+//
+                   Scanner fileScanner = new Scanner(new File("/Users/dima/Projects/FileIOJava/Step4/input.txt"));
+//
+//                System.out.println("Scanning the file");
+
+                int sumOfNumbersInsideInput = 0 ;
+
+                while (fileScanner.hasNext()) {
+
+                    //String lineIn = fileScanner.nextLine();
+                    int lineIn = fileScanner.nextInt();
+
+                    sumOfNumbersInsideInput += lineIn;
+                    System.out.println("Running total = " + sumOfNumbersInsideInput);
+                }
+                System.out.println("Reading from file done");
+
+            } catch (IOException e) {
+                System.out.println("File not find");
+            }
         }
 }
